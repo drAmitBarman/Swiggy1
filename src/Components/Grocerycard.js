76436e512cb3be70d29c8a80ff562c8cbfbd3d48
Amit Barman <ab2762314@gmail.com>
2025-08-14 19:49:49 +0530

    2nd commit

--- src/Components/Grocerycard.js
+++ src/Components/Grocerycard.js
@@ -8,5 +8,6 @@ export default function Grocerycard({foodData})
         <a href={foodData?.action?.link}>
         <img className="w-40 h-50 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+foodData?.imageId}></img>
         </a>
+        
     )
 }
